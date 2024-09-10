b7bce12e0127c70de1141154e6841cd0e748f65a
stensihver <sten.sihver@tktk.ee>
2024-09-10 16:10:50 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Net.Security;
+
+Console.WriteLine("Enter your name:");
+string username; 
+
+username = Console. ReadLine();
+
+Console.WriteLine("Hello" + ", " + username + "!");
